37863d4bc73dc3c56b0a9bdc020682d25ebe69be
Spiritx14 <Patrick9000@hotmail.ca>
2023-05-21 12:14:20 -0400

    add salut dans development

--- garage-react/src/App.js
+++ garage-react/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. allo bonjour
+          Edit <code>src/App.js</code> and save to reload. allo bonjour add salut dans development
         </p>
         <a
           className="App-link"
