48efd2464660986fa3221dc107785b49637d1f29
enix426 <jeanchristopheparentpaquette@gmail.com>
2023-05-21 11:25:30 -0400

    update bonjour

--- garage-react/src/App.js
+++ garage-react/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. allo
+          Edit <code>src/App.js</code> and save to reload. allo bonjour
         </p>
         <a
           className="App-link"
